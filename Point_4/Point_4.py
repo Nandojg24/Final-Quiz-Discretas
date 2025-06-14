ca39fddf0ddf7fbb1023ba9db22f01713377261d
Nandojg24 <juanferjg24@gmail.com>
2025-06-14 03:10:07 -0500

    We make the function to validate credentials

--- Point_4/Point_4.py
+++ Point_4/Point_4.py
@@ -5,5 +5,21 @@
 import re
 
 
+def validate_credentials(username, password):
+    validation = 0
+    if len(username) == 7 and re.fullmatch(r"^([0-1]\d{6}|2[0-5]\d{5})",
+                                           username):
+        return validation + 1
+    else:
+        return validation
+    if len(password) == 9 and re.match(r"^([a-zA-Z]{2})" +
+                                       re.escape(username) + "$", password):
+        return validation + 1
+    if validation == 2:
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
     pass
